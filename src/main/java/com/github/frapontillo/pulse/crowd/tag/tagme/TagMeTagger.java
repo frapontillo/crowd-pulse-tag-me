699cd0e248588d17fcb34ac4489f746e37c336ed
Francesco Pontillo <francescopontillo@gmail.com>
2015-12-03 09:39:26 +0100

    feat(TagMeTagger): add plugin configuration, add minRho param
    Close #1

--- src/main/java/com/github/frapontillo/pulse/crowd/tag/tagme/TagMeTagger.java
+++ src/main/java/com/github/frapontillo/pulse/crowd/tag/tagme/TagMeTagger.java
@@ -20,8 +20,10 @@ import com.github.frapontillo.pulse.crowd.data.entity.Message;
 import com.github.frapontillo.pulse.crowd.data.entity.Tag;
 import com.github.frapontillo.pulse.crowd.tag.ITaggerOperator;
 import com.github.frapontillo.pulse.spi.IPlugin;
-import com.github.frapontillo.pulse.spi.VoidConfig;
+import com.github.frapontillo.pulse.spi.IPluginConfig;
+import com.github.frapontillo.pulse.spi.PluginConfigHelper;
 import com.github.frapontillo.pulse.util.PulseLogger;
+import com.google.gson.JsonElement;
 import org.apache.logging.log4j.Logger;
 import retrofit.RestAdapter;
 import retrofit.RetrofitError;
@@ -33,10 +35,13 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * Tag Me Tagger.
+ * To configure the plugin, see {@link TagMeConfig}.
+ *
  * @author Francesco Pontillo
  * @see {@link "http://tagme.di.unipi.it/tagme_help.html#tagging"}
  */
-public class TagMeTagger extends IPlugin<Message, Message, VoidConfig> {
+public class TagMeTagger extends IPlugin<Message, Message, TagMeTagger.TagMeConfig> {
     public final static String PLUGIN_NAME = "tagme";
     private final static String TAG_ME_ENDPOINT = "http://tagme.di.unipi.it";
     private final static List<String> supportedLangs = Arrays.asList("IT", "EN");
@@ -48,11 +53,11 @@ public class TagMeTagger extends IPlugin<Message, Message, VoidConfig> {
         return PLUGIN_NAME;
     }
 
-    @Override public VoidConfig getNewParameter() {
-        return new VoidConfig();
+    @Override public TagMeConfig getNewParameter() {
+        return new TagMeConfig();
     }
 
-    @Override protected Observable.Operator<Message, Message> getOperator(VoidConfig parameters) {
+    @Override protected Observable.Operator<Message, Message> getOperator(TagMeConfig parameters) {
         return new ITaggerOperator(this) {
             @Override protected List<Tag> getTagsImpl(String text, String language) {
                 // get the tags
@@ -62,12 +67,14 @@ public class TagMeTagger extends IPlugin<Message, Message, VoidConfig> {
                 if (language != null && supportedLangs.contains(language.toUpperCase())) {
                     try {
                         response = getService().tag(text, language);
-                        for (TagMeResponse.TagMeAnnotation annotation : response.getAnnotations()) {
+                        // filter on annotations removing the ones less than the min rho
+                        response.getAnnotations().stream().filter(annotation -> annotation
+                                .isRhoHigherThan(parameters.getMinRho())).forEach(annotation -> {
                             Tag tag = new Tag();
                             tag.setText(annotation.getTitle());
                             tag.addSource(getName());
                             tags.add(tag);
-                        }
+                        });
                     } catch (RetrofitError e) {
                         // ignored
                         Response res = e.getResponse();
@@ -96,5 +103,34 @@ public class TagMeTagger extends IPlugin<Message, Message, VoidConfig> {
             service = restAdapter.create(TagMeService.class);
         }
         return service;
+    }
+
+    /**
+     * Configuration class for the plugin.
+     * The only parameter is {@code minRho}, that can be used as a minimum threshold to accept
+     * tags coming from the service.
+     *
+     * For instance, the following configuration will accept all tags with a rho equal or higher
+     * than 0.1:
+     * <pre>
+     *     {
+     *         "minRho": 0.1
+     *     }
+     * </pre>
+     */
+    public class TagMeConfig implements IPluginConfig<TagMeConfig> {
+        private Double minRho;
+
+        public Double getMinRho() {
+            return minRho;
+        }
+
+        public void setMinRho(Double minRho) {
+            this.minRho = minRho;
+        }
+
+        @Override public TagMeConfig buildFromJsonElement(JsonElement jsonElement) {
+            return PluginConfigHelper.buildFromJson(jsonElement, TagMeConfig.class);
+        }
     }
 }
